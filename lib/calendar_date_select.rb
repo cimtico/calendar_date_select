f3911ce6c28a0650bec7acf01e1bf49ab296d91e
derickm <cimtico@yahoo.com>
2013-12-09 11:56:30 -0600

    fixes to support rails 4

--- lib/calendar_date_select.rb
+++ lib/calendar_date_select.rb
@@ -7,8 +7,8 @@ module CalendarDateSelect
     # Thanks to http://robots.thoughtbot.com/post/159805560/tips-for-writing-your-own-rails-engine for:
     config.to_prepare do
       ActionView::Helpers::FormHelper.send(:include, CalendarDateSelect::FormHelpers)
-      ActionView::Helpers.send :include, CalendarDateSelect::FormHelpers
-      ActionView::Helpers.send :include, CalendarDateSelect::IncludesHelper
+      ActionView::Base.send :include, CalendarDateSelect::FormHelpers
+      ActionView::Base.send :include, CalendarDateSelect::IncludesHelper
     end
   end
 end
